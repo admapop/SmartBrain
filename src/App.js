21a78966f39da82a55a5d4b1756d8096495bab4c
admapop <52109936+admapop@users.noreply.github.com>
2019-07-10 22:31:22 +0200

    Search box and particles up and running

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import './App.css';
-import Navigation from './components/Navigation/Navigation'
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import Particles from 'react-particles-js';
+import {parameters} from './components/particlesjs-config'
 
+
+
+
+// const particlesOptions = {
+//   particles: {
+//     number: {
+//       value: 200,
+//       density: {
+//         enable: true,
+//         value_are: 800
+//       }
+//     },
+//     line_linked: {
+//       shadow: {
+//         enable: true,
+//         color: "#3CA9D1",
+//         blur: 5
+//       }
+//     }
+//   }
+// }
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+              params={parameters}
+            />
       <Navigation />
-      {/* <Logo />
+      <Logo />
+      <Rank />
       <ImageLinkForm />
-      <FaceRecognition /> */}
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
