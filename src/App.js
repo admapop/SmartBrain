29c5fff15a5c8265a832e072814561404d9284f7
admapop <52109936+admapop@users.noreply.github.com>
2019-07-11 21:34:58 +0200

    Initial setup for API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
@@ -6,9 +6,54 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
 import Particles from 'react-particles-js';
 import {parameters} from './components/particlesjs-config'
+import Clarifai from 'clarifai'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 
+const app = new Clarifai.App({
+  apiKey: '8819d596f1c74f8ba4d1c6fc5b8d6ae4'
+ });
+ 
+class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      input: '',
+    }
+  }
 
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
 
+  onButtonSubmit = () => {
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      console.log(response)
+      // do something with response
+    },
+    function(err) {
+      // there was an error
+    }
+    );
+    console.log('click')
+  }
+
+render() {
+  return (
+    <div className="App">
+      <Particles className='particles'
+              params={parameters}
+            />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+      <FaceRecognition />
+    </div>
+  );
+}}
+
+export default App;
 
 // const particlesOptions = {
 //   particles: {
@@ -28,20 +73,3 @@ import {parameters} from './components/particlesjs-config'
 //     }
 //   }
 // }
-
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-              params={parameters}
-            />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
-}
-
-export default App;
---
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FaceRecognition() {
+    return (
+        <div className='center'>
+            <img src={"https://samples.clarifai.com/face-det.jpg"} alt="result"/>
+        </div>
+    )
+}
+
+export default FaceRecognition
