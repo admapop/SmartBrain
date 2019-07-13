3d7ce9be932161a49cfc711995b3d7369908e4f7
admapop <52109936+admapop@users.noreply.github.com>
2019-07-13 21:20:01 +0200

    Code cleanup and routing finished

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-function Navigation ({ onRouteChange }) {
-    return (
+function Navigation ({ onRouteChange, isSignedIn }) {
+        if (isSignedIn) {
+        return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+            <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
             {/* arrow function needed to run the route change when we click not when we render */}
         </nav>
-    );
+		)
+    } else {
+        return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+			<p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+	    </nav>
+        );
+    }
 }
 
 export default Navigation
